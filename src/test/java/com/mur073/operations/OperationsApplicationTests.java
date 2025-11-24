70641397f5c2cc971c357d54e618d8aaf1f74dc9
mxasanov <intern_mxasanov@avo.uz>
2025-11-24 14:17:11 +0500

    Propject initialization

---
+++ src/test/java/com/mur073/operations/OperationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mur073.operations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OperationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
