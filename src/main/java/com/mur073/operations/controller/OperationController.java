1fbedb99b47b749026f66bf67098d9341742da0f
mxasanov <intern_mxasanov@avo.uz>
2025-11-24 17:35:44 +0500

    Test operations controller

--- src/main/java/com/mur073/operations/controller/OperationController.java
+++ src/main/java/com/mur073/operations/controller/OperationController.java
@@ -1,4 +1,15 @@
 package com.mur073.operations.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("operations")
 public class OperationController {
+    @GetMapping
+    public ResponseEntity<String> index() {
+        return ResponseEntity.ok("index");
+    }
 }
