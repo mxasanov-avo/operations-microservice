5d4bf645f02ffe7d81fe17e90a8c9af9bca2db1d
mxasanov <intern_mxasanov@avo.uz>
2025-11-26 17:13:51 +0500

    Transfer money endpoint

--- src/main/java/com/mur073/operations/controller/OperationController.java
+++ src/main/java/com/mur073/operations/controller/OperationController.java
@@ -1,15 +1,26 @@
 package com.mur073.operations.controller;
 
+import com.mur073.operations.dto.request.TransferMoneyRequestDto;
+import com.mur073.operations.service.OperationService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("operations")
+@RequiredArgsConstructor()
 public class OperationController {
+
+    final private OperationService operationService;
+
     @GetMapping
     public ResponseEntity<String> index() {
-        return ResponseEntity.ok("index");
+        return ResponseEntity.ok("Operations microservice");
+    }
+
+    @PostMapping("/transfer-money")
+    public ResponseEntity<String> transferMoney(@Validated @RequestBody TransferMoneyRequestDto request) {
+        return ResponseEntity.ok(operationService.transferMoney(request));
     }
 }
